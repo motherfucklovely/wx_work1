e1a717f944749c35e585c045143b3a399590e7d6
np <np@DESKTOP-8FM5MJT>
2019-07-05 01:54:22 +0800

    11

--- subscribe/src/main/java/org/fkjava/weixin/SubscribeApplication.java
+++ subscribe/src/main/java/org/fkjava/weixin/SubscribeApplication.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootApplication
 @ComponentScan("org.fkjava")
 @EnableJpaRepositories("org.fkjava")
-@EntityScan("org.java")
+@EntityScan("org.fkjava")
 public class SubscribeApplication implements //
 		EventListenerConfig, //
 		// 得到Spring的容器
